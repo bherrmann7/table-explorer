5de7d1f4d72d3131d1f9f13829962d091d332208
MAINOFFICE\bherrmann <bherrmann@demoulasmarketbasket.com>
2024-10-28 20:56:19 -0400

    add table schema to metadata query

--- src/db.clj
+++ src/db.clj
@@ -7,14 +7,14 @@
 
 (defn get-table-names [db]
   (map :table_name (j/with-db-metadata [md db]
-                     (j/metadata-result (.getTables md nil nil nil (into-array ["TABLE" "VIEW"]))))))
+                     (j/metadata-result (.getTables md nil (:user db) nil (into-array ["TABLE" "VIEW"]))))))
 
 
 (defn get-table-metadata [db table-name]
   (j/with-db-metadata [md db]
     {:name table-name
-     :cols          (j/metadata-result (.getColumns md nil nil table-name nil))
-     :primary-keys  (j/metadata-result (.getPrimaryKeys md nil nil table-name))
-     :imported-keys (j/metadata-result (.getImportedKeys md nil nil table-name))
-     :exported-keys (j/metadata-result (.getExportedKeys md nil nil table-name))}))
+     :cols          (j/metadata-result (.getColumns md nil (:user db) table-name nil))
+     :primary-keys  (j/metadata-result (.getPrimaryKeys md nil (:user db) table-name))
+     :imported-keys (j/metadata-result (.getImportedKeys md nil (:user db) table-name))
+     :exported-keys (j/metadata-result (.getExportedKeys md nil (:user db) table-name))}))
 
